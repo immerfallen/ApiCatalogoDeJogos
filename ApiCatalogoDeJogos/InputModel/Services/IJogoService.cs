a91eb04a2d9bc98e653bbaf2c6959332396230f4
Immerfallen <marodemelo@gmail.com>
2021-12-27 18:46:32 -0300

    wip

--- ApiCatalogoDeJogos/InputModel/Services/IJogoService.cs
+++ ApiCatalogoDeJogos/InputModel/Services/IJogoService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ApiCatalogoDeJogos.InputModel.Services
 {
-    public interface IJogoService
+    public interface IJogoService : IDisposable
     {
         Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
 
---
+++ ApiCatalogoDeJogos/Repositories/IJogoRepository.cs
@@ -0,0 +1,23 @@
+﻿using ApiCatalogoDeJogos.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoDeJogos.Repositories
+{
+    public interface IJogoRepository : IDisposable
+    {
+        Task<List<Jogo>> Obter(int pagina, int quantidade);
+
+        Task<Jogo> Obter(Guid id);
+
+        Task<Jogo> Obter(string nome, string produtora);
+
+        Task Inserir(Jogo jogo);
+
+        Task Atualizar(Jogo jogo);
+
+        Task Remover(Guid id);
+    }
+}
